0d63515b2b4d4bacd479fe39a88ff0d869ccb6a4
mwambutsa05 <mwambutsadaryce@gmail.com>
2025-06-09 11:44:44 +0200

    User Repository

--- src/main/java/org/example/healthcare/config/JwtFilter.java
+++ src/main/java/org/example/healthcare/config/JwtFilter.java
@@ -1,16 +1,23 @@
 package org.example.healthcare.config;
 
+import ch.qos.logback.classic.helpers.MDCInsertingServletFilter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 @Component
 @RequiredArgsConstructor
@@ -34,6 +41,17 @@ public class JwtFilter extends OncePerRequestFilter {
         jwttoken = authHeader.substring(7);
         username = jwtUtil.extractuserName(jwttoken);
 
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            if (jwtUtil.isTokenValid(jwttoken)) {
+                UserDetails userDetails = new User(username, "", new ArrayList<>());
+                UsernamePasswordAuthenticationToken authToken =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+
+        filterChain.doFilter(request, response);
     }
 
 }
--- src/main/java/org/example/healthcare/config/JwtUtil.java
+++ src/main/java/org/example/healthcare/config/JwtUtil.java
@@ -1,13 +1,46 @@
 package org.example.healthcare.config;
 
 
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+//import static java.security.KeyRep.Type.SECRET;
 
 @Service
 public class JwtUtil {
 
-    public String extractuserName(String jwttoken) {
-        return null;
+    private final String secret = "secret";
+
+    public String generateToken(String username) {
+         return Jwts.builder().setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 hour
+                .signWith(SignatureAlgorithm.HS256, String.valueOf(secret))
+                .compact();
     }
 
+    public String extractuserName(String jwttoken) {
+        return Jwts.parserBuilder()
+                .setSigningKey(secret)
+                .build()
+                .parseClaimsJws(jwttoken)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean isTokenValid(String jwttoken) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(secret)
+                    .build()
+                    .parseClaimsJws(jwttoken);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
 }
