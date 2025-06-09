59b67d16380801eddcf14b010656328250967a55
mwambutsa05 <mwambutsadaryce@gmail.com>
2025-06-09 16:14:17 +0200

    Login and register request

--- src/main/java/org/example/healthcare/user/User.java
+++ src/main/java/org/example/healthcare/user/User.java
@@ -1,18 +1,18 @@
 package org.example.healthcare.user;
 
+import com.nimbusds.oauth2.sdk.TokenIntrospectionSuccessResponse;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.Date;
 
 @Entity
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "Users")
@@ -26,6 +26,9 @@ public class User {
     @NotBlank(message = "Please enter your last name")
     @Size(max = 50, message ="your name should not exceed 50 charchaters" )
     private String lastName;
+    @NotBlank(message = "Please enter your password")
+    @Size(min = 5, message ="your should atleast be above 6 charchaters" )
+    private String password;
     @Column(unique = true)
     @Email
     private String email;
@@ -34,5 +37,7 @@ public class User {
     @NotBlank(message = "enter your date of birth")
     @Past(message = "your date of birth should be in the past")
     private Date date_of_birth;
+
+
 
 }
