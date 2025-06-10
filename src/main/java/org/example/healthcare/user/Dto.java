f75dd1b803ad4efe4ac8f0f20a7f6267b9d16a6f
mwambutsa05 <mwambutsadaryce@gmail.com>
2025-06-10 12:48:59 +0200

    changes in register dto and login dto

--- src/main/java/org/example/healthcare/user/Dto.java
+++ src/main/java/org/example/healthcare/user/Dto.java
@@ -1,5 +1,10 @@
 package org.example.healthcare.user;
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,9 +15,21 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Dto {
+    @NotBlank(message = "Please enter your first name")
+    @Size(max = 50, message ="your name should not exceed 50 characters" )
     private String firstName;
+    @NotBlank(message = "Please enter your last name")
+    @Size(max = 50, message ="your name should not exceed 50 charchaters" )
     private String lastName;
+    @NotBlank(message = "Please enter your password")
+    @Size(min = 5, message ="your password should atleast be above 6 characters" )
+    private String password;
+    @Column(unique = true)
+    @Email
     private String email;
+    @Size(max = 20, message = "your number should not exceed 20 numbers")
     private String phone;
+    @NotBlank(message = "enter your date of birth")
+    @Past(message = "your date of birth should be in the past")
     private Date date_of_birth;
 }
